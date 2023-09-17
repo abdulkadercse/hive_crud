32686a412dfe5bec986e8da38e8f93a2d7d7b44c
abdulkadercse <kaderhosen2002@@gmail.com>
2023-09-17 21:14:48 +0600

    hive crud

---
+++ android/app/src/main/kotlin/com/abdulkader/hivecrud/hive_crud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abdulkader.hivecrud.hive_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'model/model.dart';
+import 'screens/read_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(DataAdapter());
+  await Hive.openBox('data_box');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: ReadScreen()),
+    );
+  }
+}
